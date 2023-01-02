550ed4da9272d1f758bfc57d0c0baf1e251a9efe
geochronology <54387644+geochronology@users.noreply.github.com>
2023-01-02 15:41:39 -0600

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color and updates when clicked', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // find an el with role of button and text 'Change to blue'
+  const colorButton = screen.getByRole('button', {
+    name: 'Change to blue',
+  });
+
+  // expect be to be red
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect bg to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue' });
+
+  // expect button text to be 'Change to red'
+  expect(colorButton).toHaveTextContent('Change to red');
+});
+
+test('button turns blue when clicked', () => {
+  render(<App />);
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+});
+
+test('initial conditions', () => {
+  render(<App />);
+
+  // check that button starts out enabled
+  const colorButton = screen.getByRole('button', { name: 'Change to blue' });
+  expect(colorButton).toBeEnabled();
+
+  // check that checkbox starts out unchecked
+  const checkbox = screen.getByRole('checkbox');
+  expect(checkbox).not.toBeChecked();
 });
